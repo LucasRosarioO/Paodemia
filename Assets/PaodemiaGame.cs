e55ef036762c45f5287c5a98ab56eec554eaa9ac
Lucas Rosario <lucas.rosario25@gmail.com>
2020-07-29 23:36:47 -0300

    Amassadeira, Tigela Inox
    Amassadeira, Tigela Inox

--- Assets/PaodemiaGame.cs
+++ Assets/PaodemiaGame.cs
@@ -23,6 +23,12 @@ public class PaodemiaGame : MonoBehaviour
     public double BatedeiraGanchoPoder;
     public int BatedeiraGanchoNivel;
 
+    //Tigela Inox variáveis
+    public Text TigelaInoxText;
+    public double TigelaInox;
+    public double TigelaInoxCusto;
+    public double TigelaInoxPoder;
+    public int TigelaInoxNivel;
 
     //Panificadora variáveis
     public Text PanificadoraText;
@@ -30,6 +36,13 @@ public class PaodemiaGame : MonoBehaviour
     public double PanificadoraCusto;
     public double PanificadoraPoder;
     public int PanificadoraNivel;
+
+    //Amassadeira variáveis
+    public Text AmassadeiraText;
+    public double Amassadeira;
+    public double AmassadeiraCusto;
+    public double AmassadeiraPoder;
+    public int AmassadeiraNivel;
 
     //Fermento
     public Text FermentoText;
@@ -48,15 +61,25 @@ public class PaodemiaGame : MonoBehaviour
 
         //Batedeira carregar
         BatedeiraGanchoCusto = double.Parse(PlayerPrefs.GetString("BatedeiraGanchoCusto", "10"));
-        BatedeiraGanchoPoder = double.Parse(PlayerPrefs.GetString("BadeiraGanchoPoder", "1"));
+        BatedeiraGanchoPoder = double.Parse(PlayerPrefs.GetString("BatedeiraGanchoPoder", "2"));
         BatedeiraGanchoNivel = PlayerPrefs.GetInt("BatedeiraGanchoNivel", 1);
+
+        //TigelaInox carregar
+        TigelaInoxCusto = double.Parse(PlayerPrefs.GetString("TigelaInoxCusto", "5"));
+        TigelaInoxPoder = double.Parse(PlayerPrefs.GetString("TigelaInoxPoder", "1"));
+        TigelaInoxNivel = PlayerPrefs.GetInt("TigelaInoxNivel", 1);
 
         //Panificadora carregar
         PanificadoraCusto = double.Parse(PlayerPrefs.GetString("PanificadoraCusto", "25"));
         PanificadoraPoder = double.Parse(PlayerPrefs.GetString("PanificadoraPoder", "2"));
         PanificadoraNivel = PlayerPrefs.GetInt("PanificadoraNivel", 1);
 
-        
+        //Amassadeira carregar
+        AmassadeiraCusto = double.Parse(PlayerPrefs.GetString("AmassadeiraCusto", "50"));
+        AmassadeiraPoder = double.Parse(PlayerPrefs.GetString("AmassadeiraPoder", "4"));
+        AmassadeiraNivel = PlayerPrefs.GetInt("AmassadeiraNivel", 1);
+
+
     }
 
     public void Save()
@@ -71,10 +94,20 @@ public class PaodemiaGame : MonoBehaviour
         PlayerPrefs.SetString("BatedeiraGanchoPoder", BatedeiraGanchoPoder.ToString());
         PlayerPrefs.SetInt("BatedeiraGanchoNivel", BatedeiraGanchoNivel);
 
+        //TigelaInox salvar
+        PlayerPrefs.SetString("TigelaInoxCusto", TigelaInoxCusto.ToString());
+        PlayerPrefs.SetString("TigelaInoxPoder", TigelaInoxPoder.ToString());
+        PlayerPrefs.SetInt("TigelaInoxNivel", TigelaInoxNivel);
+
         //Panificadora salvar
         PlayerPrefs.SetString("PanificadoraCusto", PanificadoraCusto.ToString());
         PlayerPrefs.SetString("PanificadoraPoder", PanificadoraPoder.ToString());
         PlayerPrefs.SetInt("PanificadoraNivel", PanificadoraNivel);
+
+        //Amassadeira salvar
+        PlayerPrefs.SetString("AmassadeiraCusto", AmassadeiraCusto.ToString());
+        PlayerPrefs.SetString("AmassadeiraPoder", AmassadeiraPoder.ToString());
+        PlayerPrefs.SetInt("AmassadeiraNivel", PanificadoraNivel);
 
     }
 
@@ -92,7 +125,7 @@ public class PaodemiaGame : MonoBehaviour
         FermentoBoost = (Fermento * 0.05) + 1;
         FermentoDisponivelText.text = "Fermento:\n" + FermentoDisponivel.ToString("F0") + " Fermentos";
         FermentoText.text = "Fermentos: " + Fermento.ToString("F0");
-        FermentoBoostText.text = FermentoBoost.ToString("F2") + " Boost";
+        FermentoBoostText.text = FermentoBoost.ToString("F2") + "x Boost";
 
 
         //Texto Pão Clique / Pães por segundo
@@ -110,12 +143,26 @@ public class PaodemiaGame : MonoBehaviour
         //Texto Botão Batedeira e Panificadora
         BatedeiraGanchoText.text = "Batedeira com Gancho\n" +
             "Custo: " + BatedeiraGanchoCusto.ToString("F0") + " pães\n" +
-            "Poder: +" + BatedeiraGanchoPoder.ToString("F0") + " Pão por clique\n" +
+            "Poder: +" + BatedeiraGanchoPoder.ToString("F0") + " Pães por clique\n" +
             "Nível: " + BatedeiraGanchoNivel;
+
+        //Texto Botão Tigela Inox
+        TigelaInoxText.text = "Tigela Inox\n" +
+            "Custo: " + TigelaInoxCusto.ToString("F0") + " pães\n" +
+            "Poder: +" + TigelaInoxPoder.ToString("F0") + " pão por clique\n" +
+            "Nível: " + TigelaInoxNivel;
+
+        //Texto Botão Panificadora
         PanificadoraText.text = "Panificadora\n" +
             "Custo: " + PanificadoraCusto.ToString("F0") + " pães\n" +
-            "Poder:  " + PanificadoraPoder + " pães por segundo\n " +
+            "Poder:  +" + PanificadoraPoder.ToString("F0") + " pães por segundo\n " +
             "Nível: " + PanificadoraNivel;
+
+        //Texto Botão Amassadeira
+        AmassadeiraText.text = "Amassadeira\n" +
+            "Custo: " + AmassadeiraCusto.ToString("F0") + " pães\n" +
+            "Poder:  +" + AmassadeiraPoder.ToString("F0") + " pães por segundo\n " +
+            "Nível: " + AmassadeiraNivel;
 
         //Gerar pão por segundo
         Paes += PaesPorSeg * Time.deltaTime;
@@ -135,13 +182,23 @@ public class PaodemiaGame : MonoBehaviour
 
         //Batedeira resetar
         BatedeiraGanchoCusto = 10;
-        BatedeiraGanchoPoder = 1;
+        BatedeiraGanchoPoder = 2;
         BatedeiraGanchoNivel = 1;
+
+        //Tigela Inox resetar
+        TigelaInoxCusto = 5;
+        TigelaInoxPoder = 1;
+        TigelaInoxNivel = 1;
 
         //Panificadora resetar
         PanificadoraCusto = 25;
         PanificadoraNivel = 1;
         PanificadoraPoder = 2;
+
+        //Amassadeira resetar
+        AmassadeiraCusto = 50;
+        AmassadeiraNivel = 1;
+        AmassadeiraPoder = 4;
 
         //Fermeto resetar
         Fermento = 0;
@@ -168,13 +225,23 @@ public class PaodemiaGame : MonoBehaviour
 
             //Batedeira resetar
             BatedeiraGanchoCusto = 10;
-            BatedeiraGanchoPoder = 1 * FermentoBoost;
+            BatedeiraGanchoPoder = 2 * FermentoBoost;
             BatedeiraGanchoNivel = 1;
+
+            //Tigela Inox resetar
+            TigelaInoxCusto = 5;
+            TigelaInoxPoder = 1 * FermentoBoost;
+            TigelaInoxNivel = 1;
 
             //Panificadora resetar
             PanificadoraCusto = 25;
             PanificadoraNivel = 1;
-            PanificadoraPoder = 2;
+            PanificadoraPoder = 2 * FermentoBoost;
+
+            //Amassadeira resetar
+            AmassadeiraCusto = 50;
+            AmassadeiraNivel = 1;
+            AmassadeiraPoder = 4 * FermentoBoost;
 
             Fermento += FermentoDisponivel;
         }
@@ -209,13 +276,38 @@ public class PaodemiaGame : MonoBehaviour
 
     }
 
+    public void ComprarTigelaInox()
+    {
+        if (Paes >= TigelaInoxCusto)
+        {
+            Paes -= TigelaInoxCusto;
+            TigelaInoxNivel++;
+            TigelaInoxCusto *= 1.15;
+            PaoValorClique += TigelaInoxPoder;
+        }
+
+    }
+
+    public void ComprarTigelaInoxMaximo()
+    {
+        while (Paes >= TigelaInoxCusto)
+        {
+            Paes -= TigelaInoxCusto;
+            TigelaInoxNivel++;
+            TigelaInoxCusto *= 1.15;
+            PaoValorClique += TigelaInoxPoder;
+        }
+
+    }
+
+
     public void ComprarPanificadora()
     {
         if (Paes >= PanificadoraCusto)
         {
             Paes -= PanificadoraCusto;
-            PaesPorSeg = PanificadoraPoder;
-            PanificadoraPoder++;
+            PaesPorSeg += PanificadoraPoder;
+            //PanificadoraPoder++;
             PanificadoraNivel++;
             PanificadoraCusto *= 1.15;
             
@@ -228,12 +320,41 @@ public class PaodemiaGame : MonoBehaviour
         while (Paes >= PanificadoraCusto)
         {
             Paes -= PanificadoraCusto;
-            PaesPorSeg = PanificadoraPoder;
-            PanificadoraPoder++;
+            PaesPorSeg += PanificadoraPoder;
+            //PanificadoraPoder++;
             PanificadoraNivel++;
             PanificadoraCusto *= 1.15;
 
         }
 
     }
+
+    public void ComprarAmassadeira()
+    {
+        if (Paes >= AmassadeiraCusto)
+        {
+            Paes -= AmassadeiraCusto;
+            PaesPorSeg += AmassadeiraPoder;
+            //AmassadeiraPoder++;
+            AmassadeiraNivel++;
+            AmassadeiraCusto *= 1.15;
+
+        }
+
+    }
+
+    public void ComprarAmassadeiraMax()
+    {
+        while (Paes >= AmassadeiraCusto)
+        {
+            Paes -= AmassadeiraCusto;
+            PaesPorSeg += AmassadeiraPoder;
+            //AmassadeiraPoder++;
+            AmassadeiraNivel++;
+            AmassadeiraCusto *= 1.15;
+
+        }
+
+    }
+
 }
